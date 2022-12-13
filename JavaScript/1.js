20350a255b25adf826fbc5dee120c791d597a09c
shahhilag <shahhilag16@gmail.com>
2022-12-13 22:28:03 +0530

    Bootstrap

---
+++ JavaScript/1.js
@@ -0,0 +1,11 @@
+// alert("Hello");
+// alert("World");
+
+// Datatypes in JavaScript
+// String Number Boolean
+
+// console.log(typeof(34));
+// console.log(typeof("Hello"));
+
+var myName=prompt("What is your name?")
+alert(myName);
---
+++ JavaScript/2.js
@@ -0,0 +1,10 @@
+var a="3";
+var b="8";
+var temp=a;
+a=b;
+b=temp;
+console.log("a is " + a);
+console.log("b is " + b);
+
+//Number,Strings,_ and $ are allowed in the javascript
+
